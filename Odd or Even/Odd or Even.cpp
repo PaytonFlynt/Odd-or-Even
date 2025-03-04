6a69797de62c9559993571e551ed711c026894f8
PaytonFlynt <pf0951950@otc.edu>
2025-03-03 18:33:51 -0600

    Add project files.

---
+++ Odd or Even/Odd or Even.cpp
@@ -0,0 +1,24 @@
+#include <iostream> //input/output
+using namespace std; //allows std input
+
+int main() {
+	int number; //store user input as number
+
+	//ask user for an integer
+	cout << "Enter any number: ";
+	cin >> number;
+
+	//check if even or odd
+	if (number % 2 == 0) {
+		//remainder of 0 is even
+		cout << number << " is even." << endl;
+
+	}
+	else {
+		//remainder isn't 0, is odd
+		cout << number << " is odd." << endl;
+
+	}
+
+	return 0;
+}
